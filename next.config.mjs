ffbcec8ed2edb167b6f7e20ee686fb9567a0303f
andys8 <andys8@users.noreply.github.com>
2024-02-24 22:52:46 +0100

    webpack limit chunks

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "out",
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 2 })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
