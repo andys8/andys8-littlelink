70a60baa71dd507e06c16028c8b4d08ec534e7f2
andys8 <andys8@users.noreply.github.com>
2024-02-24 20:16:13 +0100

    Page with link buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,30 @@
 import { SparklesCore } from "@/components/ui/sparkles";
 import React from "react";
 
+type LinkData = { title: string; href: string };
+
+const linkData: LinkData[] = [
+  { title: "GitHub", href: "https://github.com/andys8" },
+  { title: "X / Twitter", href: "https://twitter.com/_andys8" },
+  { title: "Discord", href: "https://discordapp.com/users/236926669649084426" },
+  { title: "Mail", href: "mailto:andys8@99n.de" },
+];
+
+function LinkButton({ title, href }: LinkData) {
+  return (
+    <a className="p-[3px] relative" href={href} target="_blank" rel="noopener">
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+      <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent flex items-center justify-center">
+        {title}
+      </div>
+    </a>
+  );
+}
+
 export default function Home() {
   return (
-    <div className="w-full bg-black flex flex-col items-center justify-center overflow-hidden h-screen">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20 select-none">
+    <div className="w-full bg-black flex flex-col items-center justify-center overflow-hidden absolute inset-0">
+      <h1 className="md:text-7xl text-3xl font-bold text-center text-white relative z-20 select-none">
         andys8
       </h1>
       <div className="w-[40rem] h-40 relative">
@@ -18,12 +38,18 @@ export default function Home() {
           background="transparent"
           minSize={0.4}
           maxSize={1}
-          particleDensity={1200}
+          particleDensity={600}
           className="w-full h-full"
           particleColor="#FFFFFF"
         />
 
         <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 mt-[-6rem]">
+        {linkData.map((link, i) => (
+          <LinkButton key={i} {...link} />
+        ))}
       </div>
     </div>
   );
